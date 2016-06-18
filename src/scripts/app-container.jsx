69e273bb2bf3bef012901364f2fc13b9e3703917
Felicia Chandra <chandraf@nextcapital.com>
2016-06-18 16:39:05 -0500

    Updated AppContainer.

--- src/scripts/app-container.jsx
+++ src/scripts/app-container.jsx
@@ -1,26 +1,42 @@
 const React = require('react');
 const KegContainer = require('./components/keg-container.jsx');
 
-
 const AppContainer = React.createClass({
+  getInitialState() {
+    return ({
+      gaugeValue1: 20,
+      gaugeValue2: 35
+    });
+  },
 
-    componentDidMount() {
-    },
-    componentWillUnmount() {
-    },
-    render() {
-        return (
-            <div className="keg-container container">
-                <div className="title center-align">
-                    NC <br/> Keg-o-meter
-                </div>
-                <div className="row">
-                    <KegContainer guageValue = {50.6} guageID = "guageLeft"/>
-                    <KegContainer guageValue = {40.6} guageID = "guageRight"/>
-                </div>
-            </div>
-        );
-    }
+  componentWillUnmount() {
+    // TODO: Fetch kegs from server
+  },
+
+  componentDidMount() {
+    setInterval(function() {
+      this.setState({
+        gaugeValue1: this.state.gaugeValue1 + 1,
+        gaugeValue2: this.state.gaugeValue2 + 1
+      });
+    }.bind(this), 2000);
+  },
+
+  render() {
+    return (
+      <div className="keg-container container">
+        <div className="title center-align">
+          NC <br/> Keg-o-meter
+        </div>
+        <div className="row">
+          <KegContainer guageValue = {this.state.gaugeValue1} guageID = "guageLeft"/>
+          <KegContainer guageValue = {this.state.gaugeValue2} guageID = "guageRight"/>
+        </div>
+        <div className="row">
+        </div>
+      </div>
+    );
+  }
 });
 
 module.exports = AppContainer;
