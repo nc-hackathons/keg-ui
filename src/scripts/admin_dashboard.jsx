01df4c1848063f803ea71ac88b5239da4be930cb
Chengyang Wang <wangc@nextcapital.com>
2016-06-18 16:45:08 -0500

    update admin_dashboard

--- src/scripts/admin_dashboard.jsx
+++ src/scripts/admin_dashboard.jsx
@@ -8,54 +8,6 @@ const Keg = require('./components/keg.jsx');
 const Pour = require('./components/pour.jsx');
 
 const AdminDashboard = React.createClass({
-  getInitialState: function() {
-    return {
-      beers: [],
-      beer: { id: undefined, name: undefined },
-      keg: { id: undefined }
-    };
-  },
-
-  loadDataFromServer : function() {
-    axios.get('http://10.5.50.138:4567/beers').then( function (response) {
-      console.log(response);
-    }).catch( function (response) {
-      console.log(response);
-    });
-  },
-
-  submitBatchRequest: function() {
-    axios.post('http://10.5.50.138:4567/batches', {
-      batch: {
-        beer: {
-          id: this.state.beer.id,
-          name: this.state.beer.name
-        },
-        keg: {
-          id: this.state.keg.id
-        }
-      }
-    }).then(function (response) {
-      console.log(response);
-
-    }).catch(function (response) {
-      console.log(response);
-    });
-  },
-
-  handleBeerChange: function(e) {
-    console.log(e.target.value);
-    this.setState({ beer: { name: e.target.value } });
-  },
-
-  handleKegChange: function(e) {
-    this.setState({ keg: { id: e.target.value } });
-  },
-
-  componentDidMount: function() {
-    this.loadDataFromServer();
-  },
-
   render: function() {
     return (
       <div className="adminDashboard">
@@ -94,16 +46,36 @@ const AddNewKeg = React.createClass({
   getInitialState: function() {
     return {beerName: '', kegId: ''};
   },
-  submitNewKeg: function(){
+
+  submitBatchRequest: function() {
+    axios.post('http://10.5.50.138:4567/batches', {
+      batch: {
+        beer: {
+          name: this.state.beerName
+        },
+        keg: {
+          id: this.state.kegId
+        }
+      }
+    }).then(function (response) {
+      console.log(response);
+
+    }).catch(function (response) {
+      console.log(response);
+    });
+  },
+
+  submitNewKeg: function() {
+    submitBatchRequest();
     console.log(this.state.beerName);
-    console.log(this.state.kegVolume)
+    console.log(this.state.kegId);
     console.log('clicked!');
   },
   handleBeerNameChange: function(e){
     this.setState({beerName: e.target.value});
   },
   handleKegIdChange: function(e){
-    this.setState({kegVolume: e.target.kegId});
+    this.setState({kegId: e.target.kegId});
   },
   render: function(){
     return (
@@ -142,21 +114,25 @@ const AddNewKeg = React.createClass({
 
 
 const BeerList = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  loadDataFromServer : function() {
+    axios.get('http://10.5.50.138:4567/beers').then( function (response) {
+      this.setState(data: response);
+    }).catch( function (response) {
+      console.log(response);
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadDataFromServer();
+  },
+
   render: function() {
-    let data = [
-      {
-        description: "A standard cheap beer",
-        name: "Miller Lite",
-        key: "nice",
-        id: "1"
-      },
-      {
-        description: "Great for a hot day",
-        name: "PBR",
-        key: "nice2",
-        id: "2"
-      }
-    ]
+    let data = this.state.data;
+
     const beerNodes = data.map(function(beer) {
       return (
         <BeerEntry name={beer.name} description={beer.description} key={beer.id} />
