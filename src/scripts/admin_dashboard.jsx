84045e03aa18f3845180393cd0c5bfe16567136d
pearsons <pearsons@businesslogic.com>
2016-06-18 12:33:43 -0500

    Add url routing capabilities and basic admin_dashboard page

---
+++ src/scripts/admin_dashboard.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const AdminDashboard = React.createClass({
+  render: function() {
+    return (
+      <div className="adminDashboard">
+        This is the Admin Dashboard
+      </div>
+    );
+  }
+});
+
+module.exports = AdminDashboard;
--- src/scripts/index.jsx
+++ src/scripts/index.jsx
@@ -1,8 +1,16 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const KegContainer = require('./keg-container.jsx');
+const AdminDashboard = require('./admin_dashboard.jsx');
 
-ReactDOM.render(
-  <KegContainer />,
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var Link = require('react-router').Link
+
+ReactDOM.render((
+    <Router>
+      <Route path="/" component={KegContainer} />
+      <Route path="admin_dashboard" component={AdminDashboard} />
+    </Router>),
   document.getElementById('main')
 );
