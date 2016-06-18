dbd1dc76350aa3d1c8628a3adfbd73bf8c8633d8
Chengyang Wang <wangc@nextcapital.com>
2016-06-18 15:07:21 -0500

    add axios

--- src/scripts/admin_dashboard.jsx
+++ src/scripts/admin_dashboard.jsx
@@ -1,7 +1,24 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const axios = require('axios');
 
 const AdminDashboard = React.createClass({
+  loadDataFromServer : function() {
+    axios.get('http://10.5.50.138:4567/beers').then( function (response) {
+      console.log(response)
+    }).catch( function (response) {
+      console.log(response)
+    });
+  },
+
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    this.loadDataFromServer();
+  },
+
   render: function() {
     return (
       <div className="adminDashboard">
